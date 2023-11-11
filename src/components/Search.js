9e92bfcdd4c4c5cb9803cfa80cf9a1a2f4da121a
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-11-11 19:35:14 +0000

    Update 4 files

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,33 +1,35 @@
-import { useState } from 'react';
-import RecipeResults from './RecipeResults';
-import './Search.css';
+import { useState } from "react";
+import RecipeResults from "./RecipeResults";
+import "./Search.css";
 
 const Search = () => {
-    const [searchQuery, setSearchQuery] = useState(null);
-    const [recipesData, setRecipesData] = useState(null);
+  const [searchQuery, setSearchQuery] = useState(null);
+  const [recipesData, setRecipesData] = useState(null);
+  // Crate an environment variable named REACT_APP_API_KEY
+  // https://codesandbox.io/docs/learn/environment/secrets
+  const apiKey = process.env.REACT_APP_API_KEY;
 
-    const apiKey = "PUT_API_KEY_HERE";
-
-    const handleClick = async () => {
-        const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchQuery}`);
-        const responseJSON = await response.json();
-        setRecipesData(responseJSON.results);
-    }
-
-    return (
-        <>
-            <div className="search-wrapper">
-                <input type="text" placeholder="Type ingredients..."
-                    onChange={(e) => setSearchQuery(e.target.value)} />
-                <button onClick={handleClick}>
-                    Search
-                </button>
-            </div>
-            {recipesData ? (
-                <RecipeResults recipes={recipesData} />
-            ) : null }
-        </>
+  const handleClick = async () => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchQuery}`,
     );
-}
+    const responseJSON = await response.json();
+    setRecipesData(responseJSON.results);
+  };
+
+  return (
+    <>
+      <div className="search-wrapper">
+        <input
+          type="text"
+          placeholder="Type ingredients..."
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleClick}>Search</button>
+      </div>
+      {recipesData ? <RecipeResults recipes={recipesData} /> : null}
+    </>
+  );
+};
 
 export default Search;
